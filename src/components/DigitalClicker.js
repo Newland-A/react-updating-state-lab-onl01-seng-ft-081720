8c05e01386795c21ca34862371126b85ec50634f
Newland-A <ajn252@gmail.com>
2020-12-18 12:23:47 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+  constructor() {
+    super()
+    this.state = { //sets default state
+      timesClicked: 0
+    }
+  }
+  handleClick = () => {
+    this.setState(previousState => ({// handles the click event
+      timesClicked: previousState.timesClicked + 1
+    }))
+  }
+   
+  render() {
+    return ( // listens to the click event and updates the state of timesClicked
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    )
+  }
+
+
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  };
+
+  handleChangeBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  };
+
+  handleChangeRes = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    }) // or it can be assigned by this way either leaves empty attributes intact
+    // this.setState({
+    //   settings: Object.assign({}, this.state.settings, {
+    //     video: Object.assign({}, this.state.settings.video, {
+    //       resolution: '720p'
+    //     })
+    //   })
+    // });
+  }
+
+  render() {
+    return (<div>
+      <button className="bitrate" onClick={this.handleChangeBitrate}>Change Bitrate!</button>
+      <button className="resolution" onClick={this.handleChangeRes}>Change Resolution!</button>
+    </div>
+    )
+  }
+}
